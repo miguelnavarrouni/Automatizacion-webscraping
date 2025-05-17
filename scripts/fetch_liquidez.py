f8994ea66bc991574151f3a83b3de171ea0f8492
miguelnavarrouni <miguel.navarro.i@uni.pe>
2025-05-17 02:51:18 -0500

    Corrigiendo codigo fetch_scraping

--- scripts/fetch_liquidez.py
+++ scripts/fetch_liquidez.py
@@ -6,4 +6,4 @@ from src.liquidez_scraper import fetch_liquidez_table, save_to_csv
 fecha = os.environ['FECHA']
 os.makedirs(f'data/liquidez/{fecha}', exist_ok=True)
 datos = fetch_liquidez_table()
-save_to_csv(datos, f'data/liquidez/{fecha}/datos.csv')
+save_to_csv(datos, f'data/liquidez/{fecha}')
