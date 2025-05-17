306e782bf2f6e38486d31674f1747af3a430988b
miguelnavarrouni <miguel.navarro.i@uni.pe>
2025-05-17 02:17:26 -0500

    pythopath en yml

--- scripts/fetch_liquidez.py
+++ scripts/fetch_liquidez.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.liquidez_scraper import fetch_liquidez_table, save_to_csv
 
 fecha = os.environ['FECHA']
